d76b45559988a059f2ae11ce6167246417e374f1
GeorgeSolorio <Georgemsolorio@gmail.com>
2023-10-07 12:13:41 -0700

    Refactor tests to remove duplication

--- InstanteFeed/InstanteFeedTests/RemoteFeedLoaderTests.swift
+++ InstanteFeed/InstanteFeedTests/RemoteFeedLoaderTests.swift
@@ -36,21 +36,26 @@ class HTTPClientSpy: HTTPClient {
 final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let url = URL(string: "https://another-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT()
         
         sut.load()
         
         XCTAssertNotNil(client.requestedURL)
+    }
+    
+    
+    // MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "https://another-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        
+        return (sut, client)
     }
 }
