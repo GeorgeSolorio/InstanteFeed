f81a746a3dc27dd4928a919c615a4360d477335b
GeorgeSolorio <Georgemsolorio@gmail.com>
2023-10-07 17:22:01 -0700

    Delivers invalidData error on 200 HTTP response with invalid JSON

--- InstanteFeed/InstanteFeed/Feed API/RemoteFeedLoader.swift
+++ InstanteFeed/InstanteFeed/Feed API/RemoteFeedLoader.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public enum HTTPClientResult {
-    case success(HTTPURLResponse)
+    case success(Data, HTTPURLResponse)
     case failure(Error)
 }
 
@@ -33,10 +33,10 @@ public final class RemoteFeedLoader {
     public func load(completion: @escaping (Error) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case .failure:
-                completion(.connectivity)
             case .success:
                 completion(.invalidData)
+            case .failure:
+                completion(.connectivity)
             }
         }
     }
