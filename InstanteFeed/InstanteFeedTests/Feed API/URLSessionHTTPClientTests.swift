5e424245940b73d0a14bde6daf7add3ea9818802
GeorgeSolorio <Georgemsolorio@gmail.com>
2023-10-11 19:25:28 -0700

    Move the URLSessionHTTPClient (the system under test), or "SUT" creation to a factory method. To protect our test from breaking changes

--- InstanteFeed/InstanteFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ InstanteFeed/InstanteFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -50,7 +50,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -59,7 +59,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         let url = URL(string: "http://any-url.com")!
         let expectedError = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(url: url, data: nil, response: nil, error: expectedError)
-        let sut = URLSessionHTTPClient()
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for completion")
         sut.get(from: url) { result in
@@ -78,6 +78,10 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     
     // MARK: - Helpers
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
+    
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
         private static var requestObserver: ((URLRequest) -> Void)?
