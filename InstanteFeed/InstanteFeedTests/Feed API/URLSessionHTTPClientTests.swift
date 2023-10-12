ec0e82c362fced51a75d8b2cc6da2fc2232541d4
GeorgeSolorio <Georgemsolorio@gmail.com>
2023-10-11 19:38:06 -0700

    Delivers unexpected values error when request completes with data, response and error as nil

--- InstanteFeed/InstanteFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ InstanteFeed/InstanteFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -16,10 +16,14 @@ class URLSessionHTTPClient {
         self.session = session
     }
     
+    struct UnexpectedValuesRepresentation: Error { }
+    
     public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
         session.dataTask(with: url) { _, _, error in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
             }
         }.resume()
     }
@@ -57,16 +61,32 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_failsOnError() {
         let expectedError = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: expectedError)
-        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for completion")
-        sut.get(from: anyURL()) { result in
+        makeSUT().get(from: anyURL()) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, expectedError.domain)
                 XCTAssertEqual(receivedError.code, expectedError.code)
             default:
                 XCTFail("Expected failure with error \(expectedError), got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_getFromURL_failsOnAllNilValues() {
+        URLProtocolStub.stub(data: nil, response: nil, error: nil)
+        
+        let exp = expectation(description: "Wait for completion")
+        makeSUT().get(from: anyURL()) { result in
+            switch result {
+            case .failure:
+                break
+            default:
+                XCTFail("Expected failure, got \(result) instead")
             }
             exp.fulfill()
         }
