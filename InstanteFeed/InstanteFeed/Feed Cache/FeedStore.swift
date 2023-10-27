d5a111b66770e6f55c86089c1801ecc9c42a5d45
GeorgeSolorio <Georgemsolorio@gmail.com>
2023-10-27 16:00:19 -0700

    Move `FeedStore` to its own file

---
+++ InstanteFeed/InstanteFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  InstanteFeed
+//
+//  Created by George Solorio on 10/27/23.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
