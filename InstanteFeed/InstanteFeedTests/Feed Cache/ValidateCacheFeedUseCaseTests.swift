3337701180754ee69427914704c5075501bb428c
GeorgeSolorio <Georgemsolorio@gmail.com>
2023-11-05 11:04:43 -0800

    LocalFeedLoader does not message store upon creation

---
+++ InstanteFeed/InstanteFeedTests/Feed Cache/ValidateCacheFeedUseCaseTests.swift
@@ -0,0 +1,27 @@
+//
+//  ValidateCacheFeedUseCaseTests.swift
+//  InstanteFeedTests
+//
+//  Created by George Solorio on 11/5/23.
+//
+
+import XCTest
+import InstanteFeed
+
+class ValidateCacheFeedUseCaseTests: XCTestCase {
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, store)
+    }
+}
