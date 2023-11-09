9a424f316b74b24f22070d37ee0d0ab5c9d52303
GeorgeSolorio <Georgemsolorio@gmail.com>
2023-11-08 21:23:48 -0800

    Extract hardcoded store URL from the `CodableFeedStore` production type making it an explicit dependency (passed in by the test so far)

--- InstanteFeed/InstanteFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ InstanteFeed/InstanteFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -34,7 +34,11 @@ class CodableFeedStore {
         var local: LocalFeedImage { return LocalFeedImage(id: id, description: description, location: location, url: url) }
     }
     
-    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    let storeURL: URL
+    
+    init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
     
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
         guard let data = try? Data(contentsOf: storeURL) else {
@@ -131,7 +135,8 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
-        let sut = CodableFeedStore()
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        let sut = CodableFeedStore(storeURL: storeURL)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
