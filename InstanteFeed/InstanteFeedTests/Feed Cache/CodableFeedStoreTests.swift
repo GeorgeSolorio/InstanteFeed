1dc430f38eeb4ff2d7e50309a872d3ed7c7c49e6
GeorgeSolorio <Georgemsolorio@gmail.com>
2023-11-11 07:27:25 -0800

    Insert delivers error on insertion error

--- InstanteFeed/InstanteFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ InstanteFeed/InstanteFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -55,11 +55,15 @@ class CodableFeedStore {
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
-        let encoder = JSONEncoder()
-        let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
-        let encoded = try! encoder.encode(cache)
-        try! encoded.write(to: storeURL)
-        completion(nil)
+        do {
+            let encoder = JSONEncoder()
+            let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+            let encoded = try encoder.encode(cache)
+            try encoded.write(to: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
     }
 }
 
@@ -139,6 +143,16 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
     }
     
+    func test_insert_deliversErrorOnInsertionError() {
+        let invalidStoreURL = URL(string: "invalid://store-url")!
+        let sut = makeSUT(storeURL: invalidStoreURL)
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        
+        let insertionError = insert((feed, timestamp), to: sut)
+        XCTAssertNotNil(insertionError, "Expected error on invalid store url")
+    }
+    
     // MARK: - Helpers
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
         let sut = CodableFeedStore(storeURL: storeURL ?? testSpecificStoreURL())
@@ -153,7 +167,6 @@ class CodableFeedStoreTests: XCTestCase {
         
         sut.insert(cache.feed, timestamp: cache.timestamp) { receivedError in
             insertionError = receivedError
-            XCTAssertNil(insertionError, "Expected feed to be inserted successfully", file: file, line: line)
             exp.fulfill()
         }
         
